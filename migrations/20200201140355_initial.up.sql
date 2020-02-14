c72e703a70de0eed319949eeff4ec35717e78e8e
Efim Poberezkin <efim.poberezkin@gmail.com>
2020-02-14 22:51:06 +0400

    justify "ON <operation> RESTRICT" fk options, closes #5 (#7)
    * [WIP] prohibit updates via privileges, closes #5
    
    * Revert "[WIP] prohibit updates via privileges, closes #5"
    
    This reverts commit c721021000c7eede80e8703284039f2683817559.
    
    * justify "ON <operation> RESTRICT" fk options
    
    * make corrections

--- migrations/20200201140355_initial.up.sql
+++ migrations/20200201140355_initial.up.sql
@@ -2,6 +2,9 @@ CREATE TABLE uris (
   uri text PRIMARY KEY
 );
 
+/* here and further "ON <operation> RESTRICT" referential actions make foreign key checks non deferrable,
+ * so that one cannot change data even if server database credentials are compromised
+ */
 CREATE TABLE connections (
   recipient_uri text PRIMARY KEY
     REFERENCES uris (uri)
@@ -17,7 +20,7 @@ CREATE TABLE connections (
 
 CREATE TABLE messages (
   recipient_uri text NOT NULL
-    REFERENCES connections
+    REFERENCES connections (recipient_uri)
     ON DELETE CASCADE
     ON UPDATE RESTRICT,
   id uuid NOT NULL,
